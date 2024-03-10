77a4dcd13fc371ba695e88a4054ee7d728d6f140
bhaves19h <162792561+bhaves19h@users.noreply.github.com>
2024-03-10 15:44:57 +0530

    Add files via upload

---
+++ DOM.js
@@ -0,0 +1,10 @@
+//DOCUMMENT OBJECT MODEL
+//<STYLE> TAG CONNECT HTML WITH CSS head
+//<SCRIPT> CONCET HTML WITH JS 
+console.log("hello");
+//alert("abc");
+//dom doccument =object ;object ki help se html ko doccument model form mme btana.
+console.dir(window.document);
+//to check element name 
+console.dir(document.body.childNodes[0]);
+
